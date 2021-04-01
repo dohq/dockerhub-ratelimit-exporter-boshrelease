9f6b2f797e15a64807c7de96c6708282466b3fe0
dohq <dorastone@gmail.com>
2021-04-01 12:42:43 +0900

    [mod] bump 1.0.3

--- add-blob.sh
+++ add-blob.sh
@@ -6,7 +6,7 @@ mkdir -p .downloads
 
 pushd .downloads
 
-VERSION="v1.0.1"
+VERSION="v1.0.3"
 
 curl -fLO "https://github.com/dohq/dockerhub_ratelimit_exporter/releases/download/${VERSION}/dockerhub_ratelimit_exporter_linux_x86_64.tar.gz"
 bosh add-blob --dir=${DIR} dockerhub_ratelimit_exporter_linux_x86_64.tar.gz dockerhub_ratelimit_exporter/dockerhub_ratelimit_exporter_linux_x86_64.tar.gz
